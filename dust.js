2477c4f1797f417c72d46600abac5bfd39b5c6a0
Michael Kipp <michaelkipp@outlook.com>
2018-07-19 16:03:06 -0500

    Initial commit

---
+++ dust.js
@@ -0,0 +1,67 @@
+/*
+Author: Michael Kipp
+Date: 7/19/2018
+Project: Physical art project
+*/
+
+/*
+TODO: Start
+*/
+
+var TEMPO;
+
+var padEndVectorDistance;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  width = windowWidth;
+  height = windowHeight;
+
+  smooth();
+  background(255);
+  angleMode(RADIANS);
+  collideDebug(true);
+}
+
+function draw() {
+    background(255);
+    noFill();
+}
+
+function Particle(location, color) {
+  this.location = location;
+
+  this.update = function() {
+  };
+
+  this.display = function() {
+
+  };
+}
+
+function mousePressed() {
+  if (mousePressed && (mouseButton == LEFT)) {
+
+  }
+  if (mousePressed && (mouseButton == RIGHT)) {
+
+  }
+}
+
+function mouseReleased() {
+
+}
+
+function mouseDragged() {
+
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
