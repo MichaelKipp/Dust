374c878ef8166460a7c22e8f061c6bf5d82b29f1
Michael Kipp <michaelkipp@outlook.com>
2018-07-19 20:12:13 -0500

    Added attraction

--- dust.js
+++ dust.js
@@ -20,9 +20,9 @@ function setup() {
   createCanvas(width, height);
 
   DRAG = .80;
-  NUM_PARTICLES = 10000;
+  NUM_PARTICLES = 20000;
   TOP_SPEED = 10;
-  BLAST_RADIUS = 100;
+  BLAST_RADIUS = 50;
   BUBBLE_SIZE = 10;
 
   smooth();
@@ -36,18 +36,25 @@ function setup() {
 }
 
 function draw() {
-    background(255);
-    noFill();
+  background(255);
+  noFill();
 
-    particles.forEach(function(particle) {
+  particles.forEach(function(particle) {
     particle.update(mouseX, mouseY);
     particle.display();
   });
+
+  if(mouseIsPressed) {
+    particles.forEach(function(particle) {
+      particle.attract();
+    });
+  }
 }
 
 function Particle(location, c) {
   this.location = location;
   this.color = c;
+  this.color = color(255, (this.location.x / width) * 255, (this.location.y / height) * 255);
   this.velocity = createVector(0, 0);
   this.acceleration;
 
@@ -59,7 +66,9 @@ function Particle(location, c) {
     this.color = color(r, g, b, a);
   };
 
-  this.setColor(255, (this.location.x / width) * 255, (this.location.y / height) * 255);
+  this.attract = function() {
+    this.velocity.add(mouseX - this.location.x, mouseY - this.location.y);
+  };
 
   this.update = function(x, y) {
     if (dist(x, y, this.location.x, this.location.y) < BLAST_RADIUS) {
